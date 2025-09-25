aeed2ae464999f62b76aa60dd06548ed6f8eaf7b
DanielYut <daniel950401@gmail.com>
2025-09-25 14:44:53 +0800

    2

--- HW4_JavaScript1/static/113305509_js_1.js
+++ HW4_JavaScript1/static/113305509_js_1.js
@@ -1,4 +1,5 @@
 let N = 98;
+let count = 0;
 let r = Math.floor(Math.random() * (N + 1)) + 1; 
 
 document.getElementById("guess").addEventListener("click", function(){
@@ -6,13 +7,15 @@ document.getElementById("guess").addEventListener("click", function(){
     if(num > 100 || num < 0){
         alert("輸入錯誤")
     }else{
+        count++;
         if(num > r){
             alert("太大了");
         }else if(num < r){
             alert("太小了");
         }else{
-            alert("猜到了");
-            r = Math.floor(Math.random() * (N + 1)) + 1; 
+            alert("猜到了 你猜了"+count+"次");
+            r = Math.floor(Math.random() * (N + 1)) + 1;
+            count = 0;
         }
         
     }
